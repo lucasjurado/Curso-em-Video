5cf34ac5ae462488ca539ca1f654a8df2a31a9ec
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-05-09 22:57:16 -0300

    new updates

---
+++ Mundo 3/ex089.py
@@ -0,0 +1,6 @@
+from random import randint
+num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+
+print(f'A lista aleatória é: {num}.')
+print(f'O maior nº é o {max(num)}.')
+print(f'O menor nº é o {min(num)}.')
