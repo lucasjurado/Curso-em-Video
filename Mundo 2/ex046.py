9740f8929f7a4041f4ab9141257eaf094f7da436
Lucas Jurado <lucas.rsjurado@gmail.com>
2020-05-10 23:47:34 -0300

    new update

---
+++ Mundo 2/ex046.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1>num2:
+    print(f'{num1} é MAIOR!')
+elif num1<num2:
+    print(f'{num2} é MAIOR!')
+else:
+    print('Os números são iguais!')
